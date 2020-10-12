c1db31835d9c7bde928d4398d68092734fdd6655
Laura Duggan <lauracduggan@hotmail.com>
2020-10-12 18:45:12 -0400

    Updates

---
+++ precache-manifest.07f13eae330866c25203ba3809f7dee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ff100bbe6909dd65a091d87039c439",
+    "url": "/Briza/index.html"
+  },
+  {
+    "revision": "4e781d19ab2364299fab",
+    "url": "/Briza/static/css/main.e6185a8c.chunk.css"
+  },
+  {
+    "revision": "8eeb202c337157771560",
+    "url": "/Briza/static/js/2.f22060e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Briza/static/js/2.f22060e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e781d19ab2364299fab",
+    "url": "/Briza/static/js/main.b3ed3ed6.chunk.js"
+  },
+  {
+    "revision": "53d1019204f3a03cff7f",
+    "url": "/Briza/static/js/runtime-main.1b279d7b.js"
+  },
+  {
+    "revision": "d4e7eb4a02b5509f394944a0086f04f9",
+    "url": "/Briza/static/media/Background.d4e7eb4a.png"
+  }
+]);
